9fa67095ed2e1df3afdbe59bf9b47bbb382aa2d6
Victor Deplasse <imad3v@gmail.com>
2015-09-29 19:57:44 +0200

    update court registration

--- ASMAE_Website/server/methods.js
+++ ASMAE_Website/server/methods.js
@@ -2,14 +2,36 @@ Meteor.methods({
 
 	//Insert a new court in the Court database
 	'insertCourt' : function(courtData){
+		var courtAddress = {
+			//name : //get user name 
+			street : courtData.street,
+			number : courtData.number,
+			box : courtData.box,
+			city : courtData.city,
+			zipCode : courtData.zipCode
+		}
+
+		var addressID = Addresses.insert(courtAddress);
 		var data = {
 			//ownerID : Meteor.userId(),
 			surface : courtData.surface,
        		courtType : courtData.courtType,
        		instructions : courtData.instructions,
-       		ownerComment : courtData.ownerComment
+       		ownerComment : courtData.ownerComment,
+       		addressID : addressID
 
+       		/*TO ADD:
+
+       		courtNumber
+       		zone
+       		mapNumber
+       		lendThisYear
+       		availability
+       		staffComment
+       		
+       		*/
 		}
+
 		return Courts.insert(data);
 	}
 
