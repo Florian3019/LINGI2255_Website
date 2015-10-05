6ff6c5c63662811a07eadd0a114d63566bc94e92
Victor Deplasse <imad3v@gmail.com>
2015-10-05 11:30:46 +0200

    fixed collections.js

---
+++ ASMAE_Website/collections.js
@@ -0,0 +1,3 @@
+UserList = new Mongo.Collection('userlist');
+Courts = new Mongo.Collection('Courts');
+Addresses = new Mongo.Collection('Addresses');
--- ASMAE_Website/server/ASMAE_Website.js
+++ ASMAE_Website/server/ASMAE_Website.js
@@ -1,7 +1,3 @@
-UserList = new Mongo.Collection('userlist');
-Courts = new Mongo.Collection('Courts');
-Addresses = new Mongo.Collection('Addresses');
-
 Meteor.startup(function () {
 // code to run on server at startup
 });
