415fa36b1c4ee129c194df4f07d231b2b7d1139a
Aurelien <aurelien.pignolet@gmail.com>
2015-10-08 11:28:29 +0200

    Add research player

--- ASMAE_Website/client/javascript/PlayerInfo.js
+++ ASMAE_Website/client/javascript/PlayerInfo.js
@@ -1,23 +1,28 @@
+var cursor;
+Template.playersInfo.events({
+   'submit form': function(){
+      event.preventDefault();
+        var lastName = event.target.lastname.value;
+        var firstName = event.target.firstname.value;
+        var email = event.target.email.value;
+        var sex = event.target.sex.value;
+        var query = {};
+        if(lastName){query["profile.lastName"] = lastName;}
+        if(firstName){query["profile.firstName"] = firstName;}
+        if(email){query["profile.emails"] = email;}
+        if(sex) {query["profile.gender"] = sex;}
+        cursor = Meteor.users.find(query).fetch();
+        console.log(cursor);
+        Session.set('cursor', cursor);
+        return false;
+        //Router.go('courtInfo');
+    },  
+    'click li': function(){
+        console.log("You clicked an li element");
+    }
+});
 Template.playersInfo.helpers({
-  //Don't work
-  'nom': function(){
-    var player = users.find({username : "Amalingreau"}).fetch();
-    player=null;
-    if (player==null) {
-      return "Combéfis";
+    'player': function(){
+        return Session.get('cursor');
     }
-    else {
-      return "default name";
-    }
-  },
-  'prenom': function(){
-    var player = users.find({username : "Amalingreau"}).fetch();
-    player=null;
-    if (player==null) {
-      return "Sébastien";
-    }
-    else {
-      return "default first name";
-    }
-  }
 });
