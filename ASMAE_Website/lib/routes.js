dac749ff64687c9636f59da09fe896d402c3208a
Florian Demesmaeker <florian.demesmaeker@student.uclouvain.be>
2015-10-09 13:07:40 +0200

    prevents user to go to the confirmation registration page if not after registration

--- ASMAE_Website/lib/routes.js
+++ ASMAE_Website/lib/routes.js
@@ -2,6 +2,12 @@ Router.configure({
     layoutTemplate: 'index'
 });
 
+// onStop hook is executed whenever we LEAVE a route
+Router.onStop(function(){
+  // register the previous route location in a session variable
+  Session.set("previousLocationPath",Router.current().route.getName());
+});
+
 Router.route('/', {
     template: 'home',
     name: 'home'
@@ -75,7 +81,16 @@ Router.route('/brackets', {
 	name: 'brackets',
 	template: 'brackets'
 });
+
 Router.route('/confirmation_registration_player', {
 	name: 'confirmation_registration_player',
-	template: 'confirmation_registration_player'
+	template: 'confirmation_registration_player',
+	onBeforeAction: function() {
+		var previousLocationPath=Session.get("previousLocationPath");
+		// Redirect to Home if we are not coming from the tournament registration page
+		if(previousLocationPath!="tournamentRegistration"){
+			this.redirect("/")
+		}
+		this.next();
+	}
 });
