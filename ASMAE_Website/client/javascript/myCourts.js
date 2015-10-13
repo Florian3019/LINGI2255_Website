aa3e5dce6c5b52ba29ee317acae35f64815710b5
Victor Deplasse <imad3v@gmail.com>
2015-10-13 19:13:56 +0200

    small modif

--- ASMAE_Website/client/javascript/myCourts.js
+++ ASMAE_Website/client/javascript/myCourts.js
@@ -4,6 +4,11 @@ Template.myCourts.helpers({
     	return Courts.find({ownerID: Meteor.userId()});
   	},
 
+    'courtAddress': function(addressID){
+      var addr = Addresses.findOne({_id: addressID});
+      return addr.street + ", " + addr.number;
+    },
+
   	'checked': function(){
   		if(this.lendThisYear){
   			return "glyphicon-ok lendOk"
