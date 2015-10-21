3fb97ec96d29d451693a6ed2151189c577171f41
Thomas Bollen <bollenthomas1@gmail.com>
2015-10-21 14:50:24 +0200

    Add registration in pairs

---
+++ ASMAE_Website/client/javascript/confirmPair.js
@@ -0,0 +1,21 @@
+Template.confirmPair.helpers({
+	'getNameOtherPlayer' : function(idPair){
+
+		var pair = Pairs.findOne({_id:idPair});
+
+		if(!pair){
+			return "undefined pair";
+		}
+		
+		var  idPlayer= pair.player1._id;
+
+		Session.set("pair", pair);
+
+		if(idPlayer){
+			return Meteor.users.findOne({_id:idPlayer}).profile.firstName + " " + Meteor.users.findOne({_id:idPlayer}).profile.lastName;
+		}
+		else{
+			return "undefined";
+		}
+	}
+});
