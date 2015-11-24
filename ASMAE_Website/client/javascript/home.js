d5724cb520435368eb403a76b9189c4bfd870f89
Guillaume Leurquin <guillaume.leurquin@student.uclouvain.be>
2015-11-24 17:21:02 +0100

    fixed returning to normal when clicking inscription tournoi

--- ASMAE_Website/client/javascript/home.js
+++ ASMAE_Website/client/javascript/home.js
@@ -1,21 +1,21 @@
-Template.home.events({
+// Template.home.events({
 
-	'click #becomeAdmin' : function(event){
-		console.log("You are now admin");
-		Meteor.users.update({_id:Meteor.userId()}, {$set: {"profile.isAdmin":true}});
-		Meteor.users.update({_id:Meteor.userId()}, {$set: {"profile.isStaff":false}});
-	},
+// 	'click #becomeAdmin' : function(event){
+// 		console.log("You are now admin");
+// 		Meteor.users.update({_id:Meteor.userId()}, {$set: {"profile.isAdmin":true}});
+// 		Meteor.users.update({_id:Meteor.userId()}, {$set: {"profile.isStaff":false}});
+// 	},
 
-	'click #becomeStaff' : function(event){
-		console.log("You are now staff");
-		Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.isStaff":true}});
-		Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.isAdmin":false}});
-	},
+// 	'click #becomeStaff' : function(event){
+// 		console.log("You are now staff");
+// 		Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.isStaff":true}});
+// 		Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.isAdmin":false}});
+// 	},
 
-	'click #inscriptionTournoi' : function(event){
-		console.log("You are now normal");
-		Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.isStaff":false}});
-		Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.isAdmin":false}});
-	},
+// 	'click #inscriptionTournoi' : function(event){
+// 		console.log("You are now normal");
+// 		Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.isStaff":false}});
+// 		Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.isAdmin":false}});
+// 	},
 
-});
+// });
