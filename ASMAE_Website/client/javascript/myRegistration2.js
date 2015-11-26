06f52663a249d19bc8d7a9fd3a92387ac9db83b5
Aurelien <aurelien.pignolet@gmail.com>
2015-11-26 18:52:57 +0100

    desinscription +- ok

--- ASMAE_Website/client/javascript/myRegistration2.js
+++ ASMAE_Website/client/javascript/myRegistration2.js
@@ -42,8 +42,31 @@ Template.myRegistration2.helpers({
         var p2 = Meteor.users.findOne({'_id':pair.player1._id})
         str = ", Partenaire: "+player2.profile.firstName+" "+player2.profile.lastName
       }
-      result.push("Type: "+obj[i][0]+", Catégorie: "+obj[i][2]+str)
+      //result.push("Type: "+obj[i][0]+", Catégorie: "+obj[i][2]+str)
+      var str_return = "Type: "+obj[i][0]+", Catégorie: "+obj[i][2]+str
+      ret_obj = {'id': pair_id,'value':str_return}
+      result.push(ret_obj)
     }
+    console.log(result)
     return result;
+  }
+});
+Template.myRegistration2.events({
+  'click .playerFound': function(event) {
+    if(confirm("Voulez vous vous désinscrire ?")){
+      var pair_id = event.currentTarget.dataset.pairid
+      var pair = Pairs.findOne({'_id':pair_id});
+      console.log(pair)
+      if(pair.player2 == undefined){
+        Pairs.remove({'_id':pair_id});
+      }
+      else {
+        if(pair.player1._id == Meteor.userId()){
+          Pairs.update({'_id': pair_id}, {$set: {player1: pair.player2}});
+        }
+        Pairs.update({'_id': pair_id}, {$unset: {player2: ""}});
+      }
+      Router.go('home');
+    }
   }
 });
