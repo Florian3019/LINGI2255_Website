f070b518ff4aae56d2b60f2805dc61ea9fc10efe
Thomas Bollen <bollenthomas1@gmail.com>
2015-11-27 11:06:57 +0100

    getOrder in constant.js

--- ASMAE_Website/client/javascript/selectCourts.js
+++ ASMAE_Website/client/javascript/selectCourts.js
@@ -12,41 +12,6 @@ var getCourtNumbers = function(courts){
 	return result;
 }
 
-var getOrder = function(size){
-
-  var partial = function(n ,ni,result){
-
-    var half = result.length/2;
-
-    for(var i=0;i<ni;i++){
-      result[ni+i] = result[i]+n;
-      result[half+ni+i] = result[half+i]+n;
-    }
-  }
-
-  var result=[];
-
-  for(var k=0;k<size;k++){
-    if(k==size/2){
-      result.push(1);
-    }
-    else{
-      result.push(0);
-    }
-  }
-
-  var n=size/2;
-  var ni=1;
-
-  while(n>1){
-    partial(n,ni,result);
-    n=n/2;
-    ni=ni*2;
-  }
-
-  return result;
-}
-
 var checkNumberCourts = function(courtsSat,courtsSun,poolsSat,poolsSun){
 
 	if(courtsSat.length<poolsSat.length){
